b7ff83b56d89b3fe7b67c3334f52d4381b722112
DwiWahyu1204 <133838235+DwiWahyuu1204@users.noreply.github.com>
2023-09-29 00:23:12 +0700

    revisi jadi asset_dialog_buttomSheet_navigasi

--- Minggu_6/assets_dialog_bottomSheet_navigasi/praktikum/asset_dialog_bottomsheet_navigasi/lib/main.dart
+++ Minggu_6/assets_dialog_bottomSheet_navigasi/praktikum/asset_dialog_bottomsheet_navigasi/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:asset_dialog_bottomsheet_navigasi/eksplorasi/galery_eksplorasi.d
 import 'package:asset_dialog_bottomsheet_navigasi/eksplorasi/new_page_eksplorasi.dart';
 import 'package:flutter/material.dart';
 
+import 'eksplorasi advance form/home_eksplorasi.dart';
 import 'prioritas_1_2/galery_prioritas1_2.dart';
 import 'prioritas_1_2/new_page.dart';
 
@@ -18,7 +19,8 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         initialRoute: '/',
         routes: {
-          '/': (context) => GalleryPage(),
+          '/': (context) => const HomeEksplorasi(),
+          'galery': (context) => GalleryPage(),
           '/new_page': (context) => const NewPage(),
           '/GaleryEksplorasi': (context) => GaleryEksplorasi(),
           '/NewPage_eksplorasi': (context) => const NewPageEksplorasi(),
